6a466a83cb0062e35c740d705cd7de238d86773c
dthoma6403 <dthoma6403@gmail.com>
2016-09-16 12:23:43 -0700

    Added Apple Script, modified AppleTree fields to adjust movement behavior

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -21,15 +21,35 @@ public class AppleTree : MonoBehaviour {
 
 	// Use this for initialization drop apples every second
 	void Start () {
-	
+	//drop apples ever second
+		InvokeRepeating( "DropApple", 2f, secondsBetweenAppleDrops );
 	}
 	
-	// Update is called once per frame basic movement and changing direction
+	void DropApple() {
+		GameObject apple = Instantiate( applePrefab ) as GameObject;
+		apple.transform.position = transform.position;
+	}
+
+
 	void Update () {
-		// basic movement
+		// Basic Movement
 		Vector3 pos = transform.position;
 		pos.x += speed * Time.deltaTime;
 		transform.position = pos;
-	
+		// Changing Direction
+		if ( pos.x < -leftAndRightEdge ) {
+			speed = Mathf.Abs(speed);  // Move right
+		} else if ( pos.x > leftAndRightEdge ) {
+			speed = -Mathf.Abs(speed); // Move left
+		}
 	}
+	
+	void FixedUpdate() {
+		// Changing Direction Randomly
+		if ( Random.value < chanceToChangeDirections ) {
+			speed *= -1;  // Change direction
+		}
+	}
+
 }
+
