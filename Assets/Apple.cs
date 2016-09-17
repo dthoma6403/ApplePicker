6f2b4ec8c0bfda862b5c583c1f28a0aa432f8035
dthoma6403 <dthoma6403@gmail.com>
2016-09-17 16:23:57 -0700

    Apple kill command

--- Assets/Apple.cs
+++ Assets/Apple.cs
@@ -2,14 +2,11 @@
 using System.Collections;
 
 public class Apple : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+	public static float     bottomY = -20f;
 	
-	}
-	
-	// Update is called once per frame
 	void Update () {
-	
+		if ( transform.position.y < bottomY ) {
+			Destroy( this.gameObject );
+		}
 	}
 }
